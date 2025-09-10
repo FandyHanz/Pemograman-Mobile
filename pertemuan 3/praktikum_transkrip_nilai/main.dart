21b6292b90fd47a4e56dbca2bc0e3e8e779cd90f
FandyHanz <hanzyura28@gmail.com>
2025-09-10 23:37:11 +0700

    Update: CLI update

--- pertemuan 3/praktikum_transkrip_nilai/main.dart
+++ pertemuan 3/praktikum_transkrip_nilai/main.dart
@@ -7,42 +7,46 @@ List<MataKuliah> transkrip = [
   MataKuliah('IF103', 'Pemrograman', 4, 3.0),
 ];
 void main() {
- bool isCont = true;
- while (isCont) {
+  bool isCont = true;
+  while (isCont) {
+    print('===============================');
     print('Masukan opsi yang diperlukan: ');
-  print('1. hitung ipk ');
-  print('2. melihat nilai terbagus');
-  print('3. nilai terformat');
-  print('type another to exit: ');
-  String? option = stdin.readLineSync();
-  if (option == '1') {
-    hitungNilai();
-  } else if (option == '2') {
-    nilaiBagus();
-  } else if (option == '3') {
-    nilaiTerformat();
-  } else {
-    isCont = false;
-  }
- }
-}
- void hitungNilai() {
-    double totalNilai =
-        transkrip.map((mk) => mk.nilai * mk.sks).reduce((a, b) => a + b);
-    int totalSks = transkrip.map((mk) => mk.sks).reduce((a, b) => a + b);
-    double ipk = totalNilai / totalSks;
-    print('IPK: ${ipk.toStringAsFixed(2)}');
-  }
-
-  void nilaiBagus() {
-    var nilaiBagus = transkrip.where((mk) => mk.nilai >= 3.5);
-    print('\nMata kuliah dengan nilai bagus:');
-    for (var mk in nilaiBagus) {
-      print('${mk.nama} (${mk.nilai})');
+    print('1. hitung ipk ');
+    print('2. melihat nilai terbagus');
+    print('3. nilai terformat');
+    print('type another to exit: ');
+    print('===============================');
+    String? option = stdin.readLineSync();
+    if (option == '1') {
+      hitungNilai();
+    } else if (option == '2') {
+      nilaiBagus();
+    } else if (option == '3') {
+      nilaiTerformat();
+    } else {
+      isCont = false;
     }
   }
+}
 
-  void nilaiTerformat() {
-    print('\nDaftar Ringkas:');
-    transkrip.map((mk) => '${mk.nama}: ${mk.nilai}').forEach(print);
+void hitungNilai() {
+  double totalNilai = transkrip
+      .map((mk) => mk.nilai * mk.sks)
+      .reduce((a, b) => a + b);
+  int totalSks = transkrip.map((mk) => mk.sks).reduce((a, b) => a + b);
+  double ipk = totalNilai / totalSks;
+  print('IPK: ${ipk.toStringAsFixed(2)}');
+}
+
+void nilaiBagus() {
+  var nilaiBagus = transkrip.where((mk) => mk.nilai >= 3.5);
+  print('\nMata kuliah dengan nilai bagus:');
+  for (var mk in nilaiBagus) {
+    print('${mk.nama} (${mk.nilai})');
   }
+}
+
+void nilaiTerformat() {
+  print('\nDaftar Ringkas:');
+  transkrip.map((mk) => '${mk.nama}: ${mk.nilai}').forEach(print);
+}
