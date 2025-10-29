0fc42fa4962a4cf38cdb484464c069f0fc8f8511
FandyHanz <hanzyura28@gmail.com>
2025-10-29 11:51:02 +0700

    Add: UTS_Mobile continue MYDJ app

--- Pertemuan 5/lib/pages/lihat_info_aplikasi.dart
+++ Pertemuan 5/lib/pages/lihat_info_aplikasi.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class LihatInfoAplikasi extends StatefulWidget {
   LihatInfoAplikasi({super.key, required this.title});
@@ -12,6 +13,13 @@ class LihatInfoAplikasi extends StatefulWidget {
 }
 
 class _LihatInfoAplikasi extends State<LihatInfoAplikasi> {
+  final Uri _url = Uri.parse('https://fandyhanz.vercel.app');
+  Future<void> _launchUrl() async {
+    if (!await launchUrl(_url)) {
+      throw Exception('could not launch $_url');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,9 +29,78 @@ class _LihatInfoAplikasi extends State<LihatInfoAplikasi> {
       ),
       body: Center(
           child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.center,
-        children: [],
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text('MyDJ',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30,
+                      color: Colors.black))
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text('Aplikasi Jurnal Harian Guru',
+                  style: TextStyle(
+                      fontStyle: FontStyle.italic,
+                      fontSize: 20,
+                      color: Colors.black))
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text('Version: 0.1 (Beta)',
+                  style: TextStyle(fontSize: 15, color: Colors.black))
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text('Dibuat Oleh: ',
+                  style: TextStyle(fontSize: 15, color: Colors.black))
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text('Fandy Wahyu Hanzura',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                      color: Colors.black))
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              InkWell(
+                onTap: _launchUrl, // 2. Call your launch function here
+                child: const Text(
+                  // 3. This is your original Text widget
+                  'FandyHanz.vercel.app',
+                  style: TextStyle(
+                      fontSize:
+                          15, // (I kept the larger size from the last step)
+                      color: Colors.blue,
+                      decoration: TextDecoration.underline,
+                      decorationColor: Colors.blue),
+                ),
+              )
+            ],
+          )
+        ],
       )),
     );
   }
